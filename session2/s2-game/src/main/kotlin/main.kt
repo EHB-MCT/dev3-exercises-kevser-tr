03b578902faa2397dc70d4ebe0741e60d7ed636e
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-10-12 17:16:10 +0200

    challenge 2 done!!

--- session2/s2-game/src/main/kotlin/main.kt
+++ session2/s2-game/src/main/kotlin/main.kt
@@ -1,6 +1,4 @@
 
-
-
 fun main (){
     println("help,help,heeeellllpppp")
     println("help me to find my phone in this mess")
@@ -9,11 +7,11 @@ fun main (){
 
  fun challengeOne(){
      println("Where would I have left my phone? Guess out of the 3 where the location could be?")
-     val answersQuiz = mutableListOf(" the toilet", "my room", "my bf's house")
+     val answersQuiz = setOf<String>(" the toilet", "my room", "my bf's house")
      val answer = "the toilet"
      println(answersQuiz)
      val userInput= readLine()
-        if (userInput == answer ){
+        if (answersQuiz.contains(userInput) ){
             success()
             } else{
                     gameOver()
@@ -21,8 +19,22 @@ fun main (){
  }
 fun success(){
 println("sadly, you are right")
-
+challengeTwo()
 }
 fun gameOver(){
 println("YOU ARE WRONG BITCCHH")
 }
+fun challengeTwo(){
+    println("LEVEL UP, Welcome to the sec stage")
+    println("You will have to pick a number, a high and a low one")
+    val diceResult = (1..6).random()
+    val userAnswer = readLine()
+    if (userAnswer == "low" && (1..3).contains(diceResult)){
+        //win
+    }else if (userAnswer == "high" && (4..6).contains(diceResult)){
+     //win
+    }else {
+        //game-over
+    }
+}
+
